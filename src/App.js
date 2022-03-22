66d363f889e1197bf7ba323ac4e51c4492a29fa1
jaddy0 <jaddy0@yahoo.com>
2022-03-22 10:49:06 -0400

    updated welcome message in app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Changed again March 20 2022!!!
+          Changed again March 22 2022!!!
         </p>
         <a
           className="App-link"
